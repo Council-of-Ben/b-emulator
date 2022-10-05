ca48aaabbf8a5951f119798dd4bcea39822084d6
GooseterV <dangoenjoyer@gmail.com>
2022-10-05 17:41:48 -0400

    tabs :goofy_face:

--- mods/TEMPLATE.js
+++ mods/TEMPLATE.js
@@ -2,10 +2,10 @@
 // Description: A template mod.
 // Title: Template Mod
 module.exports = {
-    dependencies: [], // required mods needed for run
-    styles: true, // if false it will NOT search for a {mod}.css file
-    run: () => {
-        console.log("RUNNING FROM MOD: TEMPLATE");
-    }, // your run script, this will happen on eval
-    
+	dependencies: [], // required mods needed for run
+	styles: true, // if false it will NOT search for a {mod}.css file
+	run: () => {
+		console.log("RUNNING FROM MOD: TEMPLATE");
+	}, // your run script, this will happen on eval
+	
 };
